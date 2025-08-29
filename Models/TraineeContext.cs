3f5b43136c1af0cbf3f4b72137b0ed9528cc80b0
kusum <gkusum922@gmail.com>
2025-08-29 09:39:45 +0530

    Add project files.

---
+++ Models/TraineeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationDemoAPI.Models
+{
+    public class TraineeContext :DbContext
+    {
+        public TraineeContext(DbContextOptions<TraineeContext> options) : base(options) { }
+        
+        public DbSet<Trainee> Trainees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
